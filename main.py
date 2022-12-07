5a9bb64432b03336500d506d39d71a14beab1c22
liyuan-chang <k89070412@gmail.com>
2022-12-07 11:28:01 +0800

    blur detection using variance of the Laplacian

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from scipy import ndimage
 
 focal_stack_dir = './data/raw/'
 lapla_stack_dir = './data/laplacian/'
@@ -10,6 +11,13 @@ sharpness_dir = './data/sharpness/'
 align_dir = './data/align/'
 edge_dir = './data/edge/'
 
+# calculate sharpness by local variance of Laplacian
+def local_variance(lapla, win_rows, win_cols):
+    lapla_mean = ndimage.uniform_filter(lapla, (win_rows, win_cols))
+    lapla_sqr_mean = ndimage.uniform_filter(lapla**2, (win_rows, win_cols))
+    lapla_var = lapla_sqr_mean - lapla_mean**2
+    lapla_var = cv2.blur(lapla_var, ksize=(win_rows, win_cols))
+    return lapla_var
 
 # read focal stack
 focal_stack = []
@@ -53,14 +61,28 @@ for i, img in enumerate(focal_stack):
 
 # calculate sharpness
 lapla_stack = np.zeros((N, H, W))
+lapla_var_stack = np.zeros((N, H, W))
+win_rows, win_cols = H//20, W//20
 for i, img in enumerate(focal_stack):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     lapla = cv2.Laplacian(gray, ksize=5, ddepth=cv2.CV_32F)
     lapla = cv2.blur(lapla, ksize=(H//20, W//20))
     lapla_stack[i, :, :] = lapla
     cv2.imwrite(os.path.join(lapla_stack_dir, f'{i}.jpg'), lapla/np.max(lapla)*255)
+    # calculate sharpness by local variance of Laplacian
+    lapla_var = local_variance(lapla, win_rows, win_cols)
+    lapla_var_stack[i, :, :] = lapla_var
+    cv2.imwrite(os.path.join(lapla_stack_dir, f'var_{i}.jpg'), lapla_var/np.max(lapla_var)*255)
 
+
+plt.figure()
 sharpness = np.argmax(lapla_stack, axis=0)
 plt.imshow(sharpness)
 plt.colorbar(shrink=0.5)
 plt.savefig(os.path.join(sharpness_dir, 'sharpness.jpg'))
+
+plt.figure()
+sharpness = np.argmax(lapla_var_stack, axis=0)
+plt.imshow(sharpness)
+plt.colorbar(shrink=0.5)
+plt.savefig(os.path.join(sharpness_dir, 'sharpness_var.jpg'))
